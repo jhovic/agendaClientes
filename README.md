2447a384f9eb602713ae5d50ee112c0daa0d33ac
jhovic <joaovictorsouzaramos.jv@gmail.com>
2020-06-25 23:45:34 -0300

    adicionado opção de salvar em arquivo

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-<h1>Basic agenda to register some customer data.</h1>
-The agenda has the capacity to hold 5 clients.
-The registration consists of keeping the name, telephone and e-mail of the customers.
+<h1>Agenda basica para guardar alguns dados de clientes</h1>
+A agenda tem capacidade para guardar 5 clientes.
+O registro consiste em guardar o nome, número de telefone e e-mail de cada cliente.
--- agendaCliente.c
+++ agendaCliente.c
@@ -16,13 +16,14 @@ struct info cad[5];
 int a;
 void sair(){
 	printf("Deseja voltar ao menu?\n");
-	printf("Digite 1 para continuar cadastrando ou qualquer outro nï¿½mero para sair!\n");
+	printf("Digite 1 para continuar cadastrando ou qualquer outro número para sair!\n");
 	scanf("%d", &a);
 }
 int main(){
 	setlocale(LC_ALL, "portuguese");
 	SetConsoleOutputCP(1252);
 	SetConsoleCP(1252);
+	FILE *file;
 	int i=1;
 	int contMenu = 1;
 	int options;
@@ -35,6 +36,7 @@ int main(){
 		printf("************** MENU **************\n\n");
 		printf("1 - Inserir um novo cadastro\n");
 		printf("2 - Mostar todos os cadastros\n");
+		printf("3 - Salvar em arquivo .txt\n");
 		printf("0 - Encerrar\n\n");
 		printf("**********************************\n");
 		printf("----------------------------------\n\n");
@@ -45,7 +47,7 @@ int main(){
 				for( i; i != 6 && a == 1; i++ && cont++){
 					cad[i].cod = 1000 + rand() % 1000000;
 					printf("----------------------------------\n");
-					printf("Cliente Nï¿½ %d\n\n", i);
+					printf("Cliente %d\n\n", i);
 					printf("Digite o nome..........: ");
 					setbuf (stdin,NULL);
 					gets(cad[i].name);
@@ -75,8 +77,8 @@ int main(){
 				}else{
 					for( i = 1; i < cont; i++){
 						printf("----------------------------------\n");
-						printf("Cliente Nï¿½ %d\n\n", i);
-						printf("Cï¿½digo........: %d\n", cad[i].cod);
+						printf("Cliente %d\n\n", i);
+						printf("Código........: %d\n", cad[i].cod);
 						printf("Nome..........: %s\n", cad[i].name);
 						printf("Telefone......: %s\n", cad[i].tel);
 						printf("E-mail........: %s\n", cad[i].email);
@@ -87,12 +89,28 @@ int main(){
 				}
 				
 			break;
+			case 3:
+				file = fopen("Clientes Cadastrados.txt", "w");
+				for( i = 1; i < cont; i++){
+						fprintf(file, "----------------------------------\n");
+						fprintf(file, "Cliente %d\n\n", i);
+						fprintf(file, "Código........: %d\n", cad[i].cod);
+						fprintf(file, "Nome..........: %s\n", cad[i].name);
+						fprintf(file, "Telefone......: %s\n", cad[i].tel);
+						fprintf(file, "E-mail........: %s\n", cad[i].email);
+						fprintf(file, "----------------------------------\n\n");
+					}
+				fclose(file);
+				printf("Arquivo gerado com sucesso!\n");
+				system("pause");
+				system("cls");
+			break;
 			case 0:
 				contMenu = 0;
 			break;
 			default:
 				system("cls");
-				printf("Valor invï¿½lido!\n");
+				printf("Valor inválido!\n");
 				system("pause");
 				system("cls");
 		}
